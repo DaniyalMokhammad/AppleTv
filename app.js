0ff350429d19ec69a3851eeea89e05b2076406c9
Daniyal <daniyalmokhammad@github.com>
2018-04-03 17:00:33 -0700

    server

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
@@ -5,22 +6,26 @@ var io = require('socket.io')(server);
 
 var port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
-io.on('connection', function (socket) {
-    console.log(socket.id);
-    socket.on('start', () => {
-            console.log('update');
-            io.emit('start');
+// io.on('connection', function (socket) {
+//     console.log(socket.id);
+//     socket.on('start', () => {
+//             console.log('update');
+//             io.emit('start');
           
-      });
-    socket.on('stop', () => {
-        io.emit('stop');
-    })
-});
+//       });
+//     socket.on('stop', () => {
+//         io.emit('stop');
+//     })
+// });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+// app.listen(port, function() {
+//     console.log('Our app is running on http://localhost:' + port);
+// });
+app.get("/", function(req, res){
+    res.send("welcome to NodeJS");
+})
+app.listen(port);
