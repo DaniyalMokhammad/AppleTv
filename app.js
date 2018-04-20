d6dc8aa19f4bdc400ec78d8b7424c3f565ac3cd9
Daniyal <daniyalmokhammad@github.com>
2018-04-20 16:27:16 -0700

    updating server

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
 
+
 var express = require('express')
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-var port = process.env.PORT || 3000
+var port = 3000
+var emitters = [];
+server.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+    
+});
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
+
     console.log(socket.id);
-    socket.on('start', () => {
-            console.log('update');
-            io.emit('start');
-          
-      });
-    socket.on('stop', () => {
-        io.emit('stop');
+
+    socket.on('broadcast', function(emitName){
+        socket.broadcast.emit(emitName);
+    });
+    socket.on('sayStart', function(){
+            socket.broadcast.emit('sayStart');
     })
+
+    socket.on('connectedTV', function(){
+        console.log('Connected TV :  ' + socket.id);
+        var id = socket.id;
+        socket.broadcast.emit('connectedTV', id);
+    })
+
+    socket.on('sendingExersises', function(object){
+        console.log("Object:"  + JSON.stringify(object));
+
+        io.to(object.id).emit('recivingExercises', object.exercises);
+    })
+
+
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+
